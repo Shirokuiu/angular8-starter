1af13db8a538b65441ff4b8350b0f5aa5c548228
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2020-06-06 17:43:35 +0300

    Подправил главный модуль

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
