943fe3d11f25073f5ef0e42496725baf66cbcd90
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2020-06-06 17:57:43 +0300

    Поправил главный модуль

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared';
+import { CoreModule } from './core';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
