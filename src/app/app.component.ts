9eba210d7ef1c8cefe07e87d3fbb9ee9bcc4146d
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2020-08-08 14:41:18 +0300

    Добавил поддержку websoket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from './modules/websocket/websocket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  test: string;
+export class AppComponent implements OnInit {
+  connectStatus = 'Не подключено';
+
+  constructor(private readonly websocketService: WebsocketService) {}
+
+  ngOnInit(): void {
+    this.websocketService.on('USER').subscribe((evt) => {
+      console.log(evt);
+
+      this.connectStatus = 'Подключено';
+    });
+  }
 }
