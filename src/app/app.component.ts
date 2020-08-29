0012422c36c8fdf462098deccc6a617914e7ea86
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2020-08-29 19:53:12 +0300

    updated to angular 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { WebsocketService } from './modules/websocket/websocket.service';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +6,5 @@ import { WebsocketService } from './modules/websocket/websocket.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  connectStatus = 'Не подключено';
-
-  constructor(private readonly websocketService: WebsocketService) {}
-
-  ngOnInit(): void {
-    this.websocketService.on('USER').subscribe((evt) => {
-      this.connectStatus = 'Подключено';
-    });
-
-    this.websocketService.onDisconnect().subscribe(() => {
-      this.connectStatus = 'Не подключено';
-    });
-  }
+  ngOnInit(): void {}
 }
